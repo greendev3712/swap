a7a4e76f3eb825be18e5919bd5c0e17fec115cc5
mariosknl <mariosknl@outlook.com>
2022-09-01 20:32:34 +0300

    fix: fixed the position of the navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,7 +7,7 @@ const headerItems = ["MARKETPLACE", "MY ACCOUNT", "COLLECTION", "SWAP"];
 
 const Navbar = () => {
 	return (
-		<div className="w-[95%] rounded-3xl h-20 bg-[#f6f6f7] text-center flex justify-center items-center fixed top-5 z-10 shadow-[1px_1px_17px_1px]">
+		<div className="w-[98%] rounded-3xl h-20 bg-[#f6f6f7] text-center flex justify-center items-center absolute top-5 z-10 shadow-[1px_1px_17px_1px]">
 			{/* logo container */}
 			<div className="bg-[#242424] w-[10%] h-full rounded-xl flex justify-center items-center absolute left-0 logoContainer">
 				<Image src={Logo} alt="logo" height="60px" />
@@ -15,17 +15,28 @@ const Navbar = () => {
 			</div>
 
 			{/* Navbar Links */}
-			<div className="w-[50%] flex justify-between items-center">
+			<div className="w-[40%] flex justify-between items-center">
 				{headerItems.map((item, index) => {
 					return (
 						<Link href={`/${item.toLowerCase()}`} key={index}>
-							<a className="text-xl text-[#242424] underline underline-offset-8 underline-color decoration-[#90E040]">
+							<a className="text-lg text-[#242424] underline underline-offset-8 underline-color decoration-[#90E040]">
 								{item}
 							</a>
 						</Link>
 					);
 				})}
 			</div>
+
+			{/* Subscribe button */}
+			<button
+				type="button"
+				className="bg-[#3985F5] py-4 px-6 rounded-lg ml-20 text-white uppercase"
+			>
+				subscribe for newsletters
+			</button>
+
+			{/* User Account */}
+			<div></div>
 		</div>
 	);
 };
