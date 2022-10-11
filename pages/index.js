42e1ea8ef840b79a380cfe06ed9451b14a776840
shiwam-C114 <zaqqst@gmail.com>
2022-10-11 14:10:07 +0530

    not done

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ import WAValidator, { validate } from "multicoin-address-validator";
 import { Input } from "../components/Input";
 import { rates } from "../utils/helperFunction";
 import Navbar from "../components/Navbar";
+import { ChainId, Fetcher, Route, Trade, TokenAmount, TradeType, Percent } from "pancakeswap-v2-testnet-sdk";
+import { ethers } from "ethers";
 
 const options = ["USD", "USDT"];
 
@@ -19,6 +21,52 @@ export default function Home() {
 	const [rate, setRate] = useState(rates[0]);
 	const [ylt, setYlt] = useState(0);
 
+	async function initSwap() {
+		const chainId = ChainId.TESTNET;
+		const provider = new ethers.providers.JsonRpcProvider('https://bsctestapi.terminet.io/rpc', { name: 'binance', chainId: chainId })
+
+		const YLTtokenAddress = '0x8e0B7Ced8867D512C75335883805cD564c343cB9'
+		const USDTtokenAddress = '0x337610d27c682E347C9cD60BD4b3b107C9d34dDd'
+		const YLT = await Fetcher.fetchTokenData(chainId, YLTtokenAddress, provider)
+		const USDT = await Fetcher.fetchTokenData(chainId, USDTtokenAddress, provider)
+		const pair = await Fetcher.fetchPairData(YLT, USDT, provider)
+		const route = new Route([pair], USDT)
+		const trade = new Trade(route, new TokenAmount(USDT, "1000000000000000"), TradeType.EXACT_INPUT)
+		const slippageTolerance = new Percent('50', '1000')
+		const amountOutMin = trade.minimumAmountOut(slippageTolerance).raw;
+		const amountIn = 1e9;
+		const path = [USDT.address, YLT.address]
+		const to = '0x463B083cDefE93214b9398fEEf29C4f3C3730185'
+		const deadline = Math.floor(Date.now()/ 1000) + (60 * 20)
+		const value = trade.inputAmount.raw
+
+		let signer = new ethers.Wallet('0e38515f12388db8d5c7490d5fe659b57e3df2315086f11b416bff6a48312d52')
+		const account = signer.connect(provider)
+		let metaSigner = provider.getSigner()
+		console.log(signer, "--signer", metaSigner, "--metaSigner", account, "--account", metaAccount, "--metaAccount")
+
+		const pancakeswap = new ethers.Contract('0xCc7aDc94F3D80127849D2b41b6439b7CF1eB4Ae0', ['function swapExactTokensForTokens(uint amountIn,uint amountOutMin,address[] calldata path,address to,uint deadline) external returns (uint[] memory amounts)'], account)
+		
+		console.log(pancakeswap)
+		// const tx = await pancakeswap.swapExactTokensForTokens(amountIn,amountOutMin[1], path,to, deadline, { value, gasPrice: 20e9 })
+		// console.log(tx, tx.hash);
+
+
+		console.log(wallprovider.getSigner(), provider.getSigner());
+		// console.log(window.ethereum);
+		// MetaMask requires requesting permission to connect users accounts
+		await wallprovider.send("eth_requestAccounts", []);
+		
+		// The MetaMask plugin also allows signing transactions to
+		// send ether and pay to change state within the blockchain.
+		// For this, you need the account signer...
+		// const signer = new ethers.Wallet()
+		// const signer = provider.getSigner()
+		// console.log(signer, "signer");
+		// const account = signer.connect(provider)
+		// console.log(account);
+
+	}
 	const changeRate = () => {
 		const randomIndex = Math.floor(Math.random() * rates.length);
 		const item = rates[randomIndex];
@@ -101,11 +149,10 @@ export default function Home() {
 					placeholder="Enter your crypto wallet address"
 					value={walletAddress}
 					onChange={(e) => validateWalletAddress(e)}
-					className={`form-input font-normal text-lg ${
-						validWalletAddress
-							? "border-2 border-green-500"
-							: "border-2 border-red-500"
-					}`}
+					className={`form-input font-normal text-lg ${validWalletAddress
+						? "border-2 border-green-500"
+						: "border-2 border-red-500"
+						}`}
 				/>
 				<input
 					type="email"
@@ -115,6 +162,7 @@ export default function Home() {
 					className="form-input text-lg font-normal"
 				/>
 				<button
+					onClick={initSwap}
 					type="submit"
 					className="w-[98%] h-16 rounded-3xl bg-[#90e040] border-none text-4xl text-white uppercase mx-auto mt-20"
 				>
