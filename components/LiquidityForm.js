057f9c2a2304bfaccee9d9ce8487587ef3d16b65
Green25Dev <purplestrawberrymilk@gmail.com>
2022-11-14 01:25:20 +0900

    add if isBroswer condition

--- components/LiquidityForm.js
+++ components/LiquidityForm.js
@@ -1,53 +1,24 @@
 import { useState, useRef, useEffect } from "react";
-import { Scrolling } from "../components/Scrolling";
-import CurrencyDropdown from "../components/CurrencyDropdown";
 import "react-dropdown/style.css";
 import WAValidator from "multicoin-address-validator";
-import { rates } from "../utils/helperFunction";
-import Navbar from "../components/Navbar";
 import { useMoralis } from "react-moralis";
-import USDTLogo from '../assets/usdt.png';
-import USDLogo from '../assets/usd.png';
-import Footer from "../components/Footer";
 import Logo from "../assets/Logoemblem.svg"
-import {
-    ChainId,
-    Fetcher,
-    Route,
-    Trade,
-    TokenAmount,
-    TradeType,
-    Percent,
-    Currency, currencyEquals, ETHER, WETH
-} from "pancakeswap-v2-testnet-sdk";
-import { BigNumber, ethers } from "ethers";
-import {useRouter} from "next/router";
-
-/*--------------strat-----------------*/
-// import { BigNumber } from '@ethersproject/bignumber'
-// import { TransactionResponse } from '@ethersproject/providers'
-// // import { Currency, currencyEquals, ETHER, TokenAmount, WETH } from '@pancakeswap-libs/sdk'
-
-// import { useActiveWeb3React } from '../hooks'
-// import { useCurrency } from '../hooks/Tokens'
-// import { ApprovalState, useApproveCallback } from '../hooks/useApproveCallback'
-// import { Field } from '../state/mint/actions'
-// import { useDerivedMintInfo, useMintActionHandlers, useMintState } from '../state/mint/hooks'
-// import { useTransactionAdder } from '../state/transactions/hooks'
-// import { useIsExpertMode, useUserDeadline, useUserSlippageTolerance } from '../state/user/hooks'
-// import { calculateGasMargin, calculateSlippageAmount, getRouterContract } from '../utils_copy'
-// import { wrappedCurrency } from '../utils_copy/wrappedCurrency'
-// import useI18n from '../hooks/useI18n'
-// import { ROUTER_ADDRESS } from '../../constants'
-/*-----end-------*/
+import { ChainId } from "pancakeswap-v2-testnet-sdk";
+import { ethers } from "ethers";
+import { useRouter } from "next/router";
 
 
 const chainId = ChainId.TESTNET;
-const provider = new ethers.providers.Web3Provider(window.ethereum, { name: 'binance', chainId })
-// const provider = new ethers.providers.JsonRpcProvider(
-//     "https://bsctestapi.terminet.io/rpc",
-//     {name: "binance", chainId: chainId}
-// );
+const isBrowser = () => typeof window !== 'undefined';
+
+let web3provider;
+
+if (isBrowser()) {
+    web3provider = new ethers.providers.Web3Provider(window.ethereum, {
+        name: 'binance',
+        chainId
+    })
+}
 
 export default function LiquidityForm() {
     const validateClassNameRef = useRef('');
@@ -57,7 +28,7 @@ export default function LiquidityForm() {
     const [token_A_address, setToken_A_address] = useState("0x8e0B7Ced8867D512C75335883805cD564c343cB9")
     const [token_B_address, setToken_B_address] = useState('')
 
-    const {user, isAuthenticated} = useMoralis();
+    const { user, isAuthenticated } = useMoralis();
 
     useEffect(() => {
         if (!isAuthenticated) {
@@ -72,8 +43,8 @@ export default function LiquidityForm() {
             });
             console.log(accounts)
             const to = accounts[0]
-            await provider.send('eth_requestAccounts', []);
-            let metaSigner = provider.getSigner(to);
+            await web3provider.send('eth_requestAccounts', []);
+            let metaSigner = web3provider.getSigner(to);
             console.log(metaSigner)
 
             // factory contract
@@ -113,29 +84,7 @@ export default function LiquidityForm() {
                 metaSigner
             );
             const pairAddress = await factory.getPair(token_A_address, token_B_address)
-            // const Pair = new ethers.Contract(pairAddress,
-            //     [
-            //         "function balanceOf(address owner) external view returns (uint)"
-            //     ]
-            // )
-            console.log(token_A_address, token_B_address, "-------token address")
-            console.log(factory, token1, token2, router)
-
-            // const pairAddress = await factory.createPair.call(token_A_address, token_B_address);
-            console.log("pairAddress")
-            console.log("hello")
-            // console.log(Pair)
-            if (pairAddress == "0x0000000000000000000000000000000000000000") {
-                // pair doesnt exist
-                // create pair and
-                // const pairAddress = await factory.createPair.call(token1.address, token2.address);
-                // const tx = await factory.createPair(token_A_address, token_B_address);
-                // console.log(tx,pairAddress)
-                console.log(typeof Pair)
-
-            } else {
-                console.log("first")
-
+            if (pairAddress != "0x0000000000000000000000000000000000000000") {
                 let a = await token1.approve(router.address, token_A_value);
                 console.log(a.hash)
                 let txT_A = await isTransactionMined(a.hash)
@@ -154,29 +103,8 @@ export default function LiquidityForm() {
                     Math.floor(Date.now() / 1000) + 60 * 10
                 );
                 console.log(liq)
-                // console.log(Pair.balanceOf(to))
 
             }
-
-
-            // console.log(tx)
-            // await token1.approve(router.address, 10000);
-            // await token2.approve(router.address, 10000);
-            // let lqdt = await router.addLiquidity(
-            //     token_A_address,
-            //     token_B_address,
-            //     10000,
-            //     10000,
-            //     10000,
-            //     10000,
-            //     to,
-            //     Math.floor(Date.now() / 1000) + 60 * 10
-            // );
-            // console.log(lqdt)
-            // const pair = await Pair.at(pairAddress);
-            // const balance = await pair.balanceOf(admin);
-
-
         } catch (err) {
             console.log(err)
         }
@@ -184,7 +112,7 @@ export default function LiquidityForm() {
 
     // ----
     const isTransactionMined = async (transactionHash) => {
-        const txReceipt = await provider.getTransactionReceipt(transactionHash);
+        const txReceipt = await web3provider.getTransactionReceipt(transactionHash);
         if (txReceipt && txReceipt.blockNumber) {
             return txReceipt;
         }
@@ -202,8 +130,8 @@ export default function LiquidityForm() {
         });
         console.log(accounts)
         const to = accounts[0]
-        await provider.send('eth_requestAccounts', []);
-        let metaSigner = provider.getSigner(to);
+        await web3provider.send('eth_requestAccounts', []);
+        let metaSigner = web3provider.getSigner(to);
         // router 
         const router = new ethers.Contract(
             "0xCc7aDc94F3D80127849D2b41b6439b7CF1eB4Ae0",
@@ -247,7 +175,7 @@ export default function LiquidityForm() {
                 <div className="w-full relative mb-4">
                     <div className="absolute right-5 top-2/4 -translate-y-2/4 flex flex-col items-end">
                         <div className=" py-1.5 px-2.5 w-[134px] flex items-center rounded-3xl bg-[#C3EB9B]">
-                            <Logo className="h-6 w-6 mr-1.5"/>
+                            <Logo className="h-6 w-6 mr-1.5" />
                             <span className="text-2xl">YLT</span>
                         </div>
                         {isAuthenticated && (
@@ -274,7 +202,7 @@ export default function LiquidityForm() {
                             setToken_B_address(e.target.value)
                         }}
                         placeholder="Enter token2 address"
-                        className="form-input h-[100px] text-2xl sm:text-3xl"/>
+                        className="form-input h-[100px] text-2xl sm:text-3xl" />
                     <input
                         type="number"
                         placeholder="Enter amount"
@@ -286,7 +214,7 @@ export default function LiquidityForm() {
                     />
                 </div>
             </div>
-            { token_A_value > 0 && token_B_value > 0 && (
+            {token_A_value > 0 && token_B_value > 0 && (
                 <div>
                     YLT per USDT - {token_A_value / token_B_value}
                 </div>
