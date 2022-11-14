ca91f8fe2f476706802a1bd8b3aecc76b769e0d9
Green25Dev <purplestrawberrymilk@gmail.com>
2022-11-14 12:30:48 +0900

    stripe 3.3.9

--- pages/api/posts/stripeSuccess.js
+++ pages/api/posts/stripeSuccess.js
@@ -10,13 +10,14 @@ const YLTtokenAddress = "0x7246E5D5c4368896F0dd07794380F7e627e9AF78";
 export default async function handle(req, res) {
   const env = {
     APP_ID: "wi3vmn7KB9vehixK5lZ2vOuAfgbJzJNSjum3AkUp",
-    APP_SERVER_URL: "https://b3o7m8vdspy1.usemoralis.com:2053/server"
+    APP_SERVER_URL: "https://b3o7m8vdspy1.usemoralis.com:2053/server",
+    APP_MASTER_KEY: "zW1oIZN0Muq2OW5bBsAwsbm7pn22IJz1DJtHj2Tb"
   }
 
   if (req.method === 'POST') {
     let { status, timestamp } = req.body;
     timestamp = timestamp.replace(/ /g, '+');
-
+    await Moralis?.start({ serverUrl: env.APP_SERVER_URL, appId: env.APP_ID, masterKey: env.APP_MASTER_KEY })
     if (status != "success" || timestamp.length < 100)
       res.status(500).json({ msg: "Internal Server Error!!!" });
 
@@ -26,44 +27,42 @@ export default async function handle(req, res) {
 
     let token = decode.substr(32, 64);
 
-    Moralis.start({ serverUrl: env.APP_SERVER_URL, appId: env.APP_ID }).then(() => {
-      let id,
-        address,
-        amount;
-      Moralis.Cloud.run("getTempFile", { token }).then(async res => {
-        console.log(res);
-        id = res.id;
-        address = res.attributes.address;
-        amount = res.attributes.token_amount;
-        console.log(address);
+    let id,
+      address,
+      amount;
+    Moralis.Cloud.run("getTempFile", { token }).then(async res => {
+      console.log(res);
+      id = res.id;
+      address = res.attributes.address;
+      amount = res.attributes.token_amount;
+      console.log(address);
 
-        if (id == null || id == undefined || address == null || address == undefined || amount == null || amount == undefined)
-          res.status(500).json({ msg: 'Internal Server Error!!!' });
-
+      if (id == null || id == undefined || address == null || address == undefined || amount == null || amount == undefined)
+        res.status(500).json({ msg: 'Internal Server Error!!!' });
 
 
-        console.log('Success!!!!!!!!!!!!!');
 
-        Moralis.Cloud.run("deleteTempFile", { id: id }).then(res => { }).catch(err => console.log(err));
+      console.log('Success!!!!!!!!!!!!!');
 
-        const privateKey = 'e426d7cb74727e7c4d743c8b081bb1f3af6d280e6ab2fb10cb27581eda12387d';
+      Moralis.Cloud.run("deleteTempFile", { id: id }).then(res => { }).catch(err => console.log(err));
 
-        let wallet = new ethers.Wallet(privateKey);
+      const privateKey = 'e426d7cb74727e7c4d743c8b081bb1f3af6d280e6ab2fb10cb27581eda12387d';
 
-        // Connect a wallet to mainnet
-        let provider = new ethers.providers.JsonRpcProvider("https://data-seed-prebsc-1-s3.binance.org:8545");
+      let wallet = new ethers.Wallet(privateKey);
 
-        let walletWithProvider = new ethers.Wallet(privateKey, provider);
+      // Connect a wallet to mainnet
+      let provider = new ethers.providers.JsonRpcProvider("https://data-seed-prebsc-1-s3.binance.org:8545");
 
-        const YLTContract = new ethers.Contract(YLTtokenAddress, YLTABI, walletWithProvider);
-        console.log('Contracyt0');
-        let tx = await YLTContract.transfer(address, ethers.utils.parseUnits(Number(amount).toString(), 18));
-        await tx.wait();
-        console.log(tx.hash);
+      let walletWithProvider = new ethers.Wallet(privateKey, provider);
 
-      }).catch(err => console.log(err));
+      const YLTContract = new ethers.Contract(YLTtokenAddress, YLTABI, walletWithProvider);
+      console.log('Contracyt0');
+      let tx = await YLTContract.transfer(address, ethers.utils.parseUnits(Number(amount).toString(), 18));
+      await tx.wait();
+      console.log(tx.hash);
 
     }).catch(err => console.log(err));
+
 
     res.status(200).json({ msg: 'success' });
   }
